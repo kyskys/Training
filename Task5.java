21676ac67095f043a3bb5624496637607a649089
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-11-03 15:44:12 +0200

    no message

--- Task5.java
+++ Task5.java
@@ -5,11 +5,7 @@ public class Task5 {
 
 	final static Random r = new Random();
 	public static int random (){
-		String s = "";
-		for(int i = 0;i<3;++i){
-			s.concat(String.valueOf(r.nextInt(9)));
-			}
-		return Integer.valueOf(s);
+		return (int) ((Math.random()*100)+899);
 	}
 	public static void main(String[] args) {
 		System.out.println(Integer.valueOf((random()+""+random()))/random());
