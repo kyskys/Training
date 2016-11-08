0d61f4aff640e212373b0a83f4abd25761077f92
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-11-08 17:57:10 +0300

    исправляю косяк, есть же методы для этого

--- Hospital/src/test/Test.java
+++ Hospital/src/test/Test.java
@@ -1,5 +1,6 @@
 package test;
 
+import util.Operations;
 import classes.Doctor;
 import classes.Patient;
 
@@ -8,10 +9,10 @@ public class Test {
 	public static void main(String[] args) {
 		Doctor[] doctorMas = new Doctor[1];
 		Patient[] patientMas = new Patient[1];
-		doctorMas[0] = new Doctor("slaik");
-		doctorMas[0] = new Doctor("ches");
-		patientMas[0] = new Patient("podopinaya mish 1");
-		patientMas[1] = new Patient("podopintaya mish 2");
+		doctorMas[0] = Operations.addDoctor("slaik");
+		doctorMas[1] = Operations.addDoctor("ches");
+		patientMas[0] = Operations.addPatient("podopinaya mish 1");
+		patientMas[0] = Operations.addPatient("podopinaya mish 2");
 	}
 
 }
