b8bbda39176684e025f007414d8919308e6c1fd1
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-11-08 17:08:03 +0300

    добавление полей в doctor и patient (хз как оно будет общаться между собой, придумаю по ходу дел)

--- Hospital/src/classes/Doctor.java
+++ Hospital/src/classes/Doctor.java
@@ -1,9 +1,9 @@
 package classes;
-
+import java.util.ArrayList;
 import util.Role;
 
 public class Doctor extends Man{
-	
+	ArrayList<Patient> patients;
 	public Doctor (String fio,Role role){
 		super(fio);
 		super.setRole(Role.DOCTOR);
--- Hospital/src/classes/Patient.java
+++ Hospital/src/classes/Patient.java
@@ -2,10 +2,16 @@ package classes;
 import util.Role;
 
 public class Patient extends Man{
-	
+	private Doctor doctor;
 	public Patient (String fio){
 	super(fio);
 	super.setRole(Role.PATIENT);
 }
+	public Doctor getDoctor() {
+		return doctor;
+	}
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
 }
 
