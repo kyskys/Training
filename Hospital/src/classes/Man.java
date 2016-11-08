a8b3b70fb34ad71f755e2de3fca3904f0e87414c
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-11-08 16:51:52 +0300

    новые классы man и role

---
+++ Hospital/src/classes/Man.java
@@ -0,0 +1,11 @@
+package classes;
+import util.Role;
+
+public abstract class Man {
+private String fio;
+private Role role;
+public Man (String fio, Role role){
+	this.fio=fio;
+	this.role=role;
+}
+}
---
+++ Hospital/src/util/Role.java
@@ -0,0 +1,5 @@
+package util;
+
+public enum Role {
+PATIENT, DOCTOR;
+}
