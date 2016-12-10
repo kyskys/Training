a44b917bf06856b6aa1c6a16321de56ca6a0b4f4
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-12-10 18:41:53 +0300

    просто без комментариев

--- EPC/src/util/Creator.java
+++ EPC/src/util/Creator.java
@@ -4,20 +4,27 @@ import java.io.Console;
 
 import model.Course;
 import model.Lector;
+import util.Helper;
 
 public class Creator {
 	
-public void addCourse () {
+
+public void addCourse (String name, String info) {
 	Course course = new Course(name,info);
 	course.setLector(addLector());
+	
 }
 
-public void addLector () {
+public Lector addLector () {
+	Lector[] lectors = new Lector[5];//еще не придумал как брать ссылку на всех, будет пока так
 	try {
-		
+		if (lectors[0]==null) {
+			lectors[0] = new Lector("name");//создает новый, если список лекторов пуст
+		}
+		else return (Lector) Helper.selectSmtng(lectors); //че то ошибка, слаик, подскажи
 	}
-	catch {
-		System.out.println("oshibka! povtorite");
+	catch { //не хочет, пишет add finally, подскажи
+		System.out.println("oshibka, povtorite");
 		addLector();
 	}
 }
