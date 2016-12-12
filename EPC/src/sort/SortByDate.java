586b21b43315429c7ca1b92352d7a0c28c8bcb2a
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-12-12 14:07:14 +0300

    добавление пары сортировок (по дате, дате начала)

--- EPC/src/sort/SortByDate.java
+++ EPC/src/sort/SortByDate.java
@@ -1,5 +1,18 @@
 package sort;
 
-public class SortByDate {
+import java.util.Comparator;
+import java.util.Date;
+
+import model.Lection;
+
+
+public class SortByDate implements Comparator <Lection> {
+
+	@Override
+	public int compare(Lection l1, Lection l2) {
+		Date d1 = l1.getDate();
+		Date d2 = l2.getDate();
+		return d1.compareTo(d2);
+	}
 
 }
