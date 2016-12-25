2a4d3b73981ed1b0eed27a235e0cb8f73df82247
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-12-25 18:29:06 +0300

    добавление сортировки лекторов по кол. курсов, добавление ссылки на курсы в модели lector, добавление сортировок для лектора и курса

--- EPC/src/model/Lector.java
+++ EPC/src/model/Lector.java
@@ -1,10 +1,19 @@
 package model;
 
-public class Lector extends Human{
+import java.util.ArrayList;
 
+public class Lector extends Human{
+	
+	private ArrayList<Course> courses;
 	public Lector(String fio) {
 		super(fio);
 		super.setRole(Role.LECTOR);
+	}
+	public ArrayList<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(ArrayList<Course> courses) {
+		this.courses = courses;
 	}
 
 }
--- EPC/src/util/Helper.java
+++ EPC/src/util/Helper.java
@@ -3,12 +3,11 @@ package util;
 import java.util.ArrayList;
 
 import sort.*;
-import model.Course;
+import model.*;
 
 public class Helper {
 
-	public static void sortCourse (String select, ArrayList<Course> courses)
-	{
+	public static void sortCourse (String select, ArrayList<Course> courses) {
 		switch(select)
 		{
 		case("start date"):{
@@ -17,7 +16,23 @@ public class Helper {
 		case("students count"):{
 			courses.sort(new SortCourseByStudentsCount());
 		}
-		
+		case("alphabet"):{
+			courses.sort(new SortByName());
 		}
+		}
+	}
+	public static void sortLector (String select, ArrayList<Lector> lectors) {
+		switch(select)
+		{
+		case("alphabet"): {
+			lectors.sort(new SortByName());
+		}
+		case("course count"): {
+			lectors.sort(new SortLectorByCourseCount());
+		}
+		}		
+	}
+	public static void showDescriptionOfCourse(ArrayList<Course> courses) {
+		
 	}
 }
