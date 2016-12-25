3810c0817b77306b1a2c34f8567b648993b389e9
Kowka-ПК\Kowka <free_gallos@mail.ru>
2016-12-25 18:15:39 +0300

    добавление 2х сортировок

--- EPC/src/util/Helper.java
+++ EPC/src/util/Helper.java
@@ -1,9 +1,23 @@
 package util;
 
-import model.Abstract;
-import model.Lector;
+import java.util.ArrayList;
+
+import sort.*;
+import model.Course;
 
 public class Helper {
 
-	
+	public static void sortCourse (String select, ArrayList<Course> courses)
+	{
+		switch(select)
+		{
+		case("start date"):{
+			courses.sort(new SortCourseByStartDate());
+		}
+		case("students count"):{
+			courses.sort(new SortCourseByStudentsCount());
+		}
+		
+		}
+	}
 }
