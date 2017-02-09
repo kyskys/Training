45b4603ef1f56b2202f3684d1bbf3733f700fefa
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-02-09 14:47:08 +0300

    я пилю, пилю меню

--- EPC/src/model/Human.java
+++ EPC/src/model/Human.java
@@ -1,13 +1,14 @@
 package model;
 
-public abstract class Human extends BaseModel{
-public Human (String name){
-	setName(name);
-}
-public abstract Role getRole();
+public abstract class Human extends BaseModel {
+	public Human(String name) {
+		setName(name);
+	}
 
-public enum Role {
-LECTOR, LISTENER;
-}
+	public abstract Role getRole();
+
+	public enum Role {
+		LECTOR, LISTENER;
+	}
 
 }
--- UI/src/util/MenuBuilder.java
+++ UI/src/util/MenuBuilder.java
@@ -1,4 +1,5 @@
 package util;
+
 import menu.Menu;
 import menu.MenuPoint;
 
@@ -7,11 +8,39 @@ public class MenuBuilder {
 		Menu mainMenu = new Menu("glavnoe menu");
 		Menu bd = new Menu("rabota s bd");
 		Menu show = new Menu("vivod informacii");
-		mainMenu.getList().add(new MenuPoint(bd, "menu 1"));
-		mainMenu.getList().add(new MenuPoint(show, "menu 2"));
-		bd.getList().add(new MenuPoint(mainMenu, "nazad"));
-		show.getList().add(new MenuPoint(mainMenu, "nazad"));
+		mainMenu.Add(new MenuPoint(bd, "menu 1"));
+		mainMenu.Add(new MenuPoint(show, "menu 2"));
+		mainMenu.Add(new MenuPoint(null, "vihod"));
+		Menu courseServ = new Menu("menu raboti s kyrsami");
+		courseServ.Add(new MenuPoint(mainMenu, "sozdat kyrs", null));
+		courseServ.Add(new MenuPoint(mainMenu, "ydalit kyrs", null));
+		courseServ.Add(new MenuPoint(mainMenu, "dobavit lektora k kyrsy", null));
+		courseServ.Add(new MenuPoint(mainMenu, "ydalit lektora y kyrsa", null));
+		courseServ.Add(new MenuPoint(mainMenu, "dobavit lekciu k kyrsy", null));
+		courseServ.Add(new MenuPoint(mainMenu, "ydalit lekciu y kyrsa", null));
+		courseServ.Add(new MenuPoint(mainMenu, "dobavit studenta k kyrsy", null));
+		courseServ.Add(new MenuPoint(mainMenu, "ydalit studenta y kyrsa", null));
+		Menu lectorServ = new Menu("menu raboti s lektorami");
+		lectorServ.Add(new MenuPoint(mainMenu, "sozdat lektora", null));
+		lectorServ.Add(new MenuPoint(mainMenu, "ydalit lektora", null));
+		lectorServ.Add(new MenuPoint(mainMenu, "dobavit kyrs k lektory", null));
+		lectorServ.Add(new MenuPoint(mainMenu, "ydalit kyrs y lektora", null));
+		Menu lectionServ = new Menu("menu raboti s lekciyami");
+		lectionServ.Add(new MenuPoint(mainMenu, "sozdat lekciu", null));
+		lectionServ.Add(new MenuPoint(mainMenu, "ydalit lekciu", null));
+		lectionServ.Add(new MenuPoint(mainMenu, "dobavit kyrs k lekcii", null));
+		lectionServ.Add(new MenuPoint(mainMenu, "ydalit kyrs y lekcii", null));
+		lectionServ.Add(new MenuPoint(mainMenu, "dobavit daty k lekcii", null));
+		lectionServ.Add(new MenuPoint(mainMenu, "ydalit daty y lekcii", null));
+		Menu studentServ = new Menu("menu raboti s studentami");
+		bd.Add(new MenuPoint(courseServ, "ypravlenie kyrsami"));
+		bd.Add(new MenuPoint(lectorServ, "ypravlenie lektorami"));
+		bd.Add(new MenuPoint(lectionServ, "ypravlenie lekciyami"));
+		bd.Add(new MenuPoint(studentServ, "ypravlenie studentami"));
+		bd.Add(new MenuPoint(mainMenu, "na glavnoe menu"));
+		
+		show.Add(new MenuPoint(mainMenu, "nazad"));
 		return mainMenu;
 	}
-	
+
 }
