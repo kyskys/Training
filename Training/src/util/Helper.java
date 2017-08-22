6990f2a156d16411f8166e261f9f0fa78a170b95
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-08-22 08:50:12 +0300

    cоздание класса Helper для первого задания

---
+++ Training/src/util/Helper.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Helper {
+	@SuppressWarnings("unchecked")
+	public <T extends Object> T stringParse(String str, T toParse) {
+		T result = 
+		return (T) str;
+	}
+
+}
