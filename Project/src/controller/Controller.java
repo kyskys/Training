6643b6d37de59b3e6f71ed70be5db37ff2c4dad3
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-08-24 19:43:00 +0300

    создание контроллера для программы

--- Project/src/controller/Controller.java
+++ Project/src/controller/Controller.java
@@ -1,5 +1,38 @@
 package controller;
 
-public class Controller {
+import java.util.Scanner;
 
+public class Controller {
+	static Scanner in = new Scanner(System.in);
+
+	public static void start() {
+		while (true) {
+			System.out.println("1. simple number\n2. fibonacci number\n3. line crossing"
+					+ "\n4. least common multipler / greatest common divisor\n5. palindrome"
+					+ "\n6. number removing\n7. exit");
+			int n = in.nextInt();
+			switch (n) {
+			case (1): {
+			}
+			case (2): {
+
+			}
+			case (3): {
+
+			}
+			case (4): {
+
+			}
+			case (5): {
+
+			}
+			case (6): {
+
+			}
+			case (7): {
+				break;
+			}
+			}
+		}
+	}
 }
--- Project/src/main/Main.java
+++ Project/src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import controller.Controller;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Controller.start();
 
 	}
 
