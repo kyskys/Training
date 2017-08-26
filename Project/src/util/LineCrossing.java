51ef78f14ba0febdcc9907c6e3b55cff97526853
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-08-26 10:13:09 +0300

    написаное третье задание (для чистого случая, т.е. без вариантов пересечения в одной точке и нахождения отрезков друг в друге)

--- Project/src/util/LineCrossing.java
+++ Project/src/util/LineCrossing.java
@@ -1,10 +1,21 @@
 package util;
 
 public class LineCrossing {
-	public static String evaluate(int x11, int y11, int x12, int y12, int x21, int y21, int x22, int y22) {
-		if((x11==x21&&y11==y21)||(x11==x22&&y11==y22)||(x12==x21&&y12==y21)||(x12==x22&&y12==y22)) {
-			return "crossing in edge";
+	public static boolean evaluate(double x1, double y1, double x2, double y2, double x3, double y3, double x4,
+			double y4) {
+		try {
+			double t1 = ((x2 - x1) * (y1 - y3) + (y2 - y1) * (x3 - x1))
+					/ ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+			if (0 < t1 && t1 < 1) {
+				double t2 = ((x3 - x1) + t1 * (x4 - x3)) / (x2 - x1);
+				if (0 < t2 && t2 < 1) {
+					return true;
+				} else
+					return false;
+			} else
+				return false;
+		} catch (Exception e) {
+			return false;
 		}
-		return null;
 	}
 }
