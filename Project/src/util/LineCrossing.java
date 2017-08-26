a3d85a172279ff2a173b0fcc614f1e9d1f9bc699
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-08-26 11:39:44 +0300

    дописан if для случая отрезков на одной прямой

--- Project/src/util/LineCrossing.java
+++ Project/src/util/LineCrossing.java
@@ -3,13 +3,19 @@ package util;
 public class LineCrossing {
 	public static boolean evaluate(double x1, double y1, double x2, double y2, double x3, double y3, double x4,
 			double y4) {
-		if((x1==x3&&y1==y3)||(x2==x3&&y2==y3)||(x1==x4&&y2==y4)||(x2==x4&&y2==y4)) {
+		if ((x1 == x3 && y1 == y3) || (x2 == x3 && y2 == y3) || (x1 == x4 && y1 == y4) || (x2 == x4 && y2 == y4)) {
 			return true;
 		}
-		try {
 			double t1 = ((x2 - x1) * (y1 - y3) + (y2 - y1) * (x3 - x1))
 					/ ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
-			if (0 <= t1 && t1 <= 1) {
+			if(Double.isNaN(t1)) {
+				if ((x1 < x3 && x3 < x2) || (x1 < x4 && x4 < x2) || (y1 < y3 && y3 < y2) || (y1 < y4 && y4 < y2)
+						|| (x3 < x1 && x1 < x4) || (x3 < x2 && x2 < x4) || (y3 < y1 && y1 < y4) || (y3 < y2 && y2 < y4)) {
+					return true;
+				} else
+					return false;
+			}
+			else if (0 <= t1 && t1 <= 1) {
 				double t2 = ((x3 - x1) + t1 * (x4 - x3)) / (x2 - x1);
 				if (0 <= t2 && t2 <= 1) {
 					return true;
@@ -17,8 +23,6 @@ public class LineCrossing {
 					return false;
 			} else
 				return false;
-		} catch (Exception e) {
-			return false;
-		}
+		
 	}
 }
