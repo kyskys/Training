89fc7120e98ebe245ca6338d284ed153be607098
Kowka-ПК\Kowka <free_gallos@mail.ru>
2017-08-26 10:43:31 +0300

    исправлено третье задание для случаев совпадения вершин и вершины внутри другого

--- Project/src/util/LineCrossing.java
+++ Project/src/util/LineCrossing.java
@@ -3,12 +3,15 @@ package util;
 public class LineCrossing {
 	public static boolean evaluate(double x1, double y1, double x2, double y2, double x3, double y3, double x4,
 			double y4) {
+		if((x1==x3&&y1==y3)||(x2==x3&&y2==y3)||(x1==x4&&y2==y4)||(x2==x4&&y2==y4)) {
+			return true;
+		}
 		try {
 			double t1 = ((x2 - x1) * (y1 - y3) + (y2 - y1) * (x3 - x1))
 					/ ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
-			if (0 < t1 && t1 < 1) {
+			if (0 <= t1 && t1 <= 1) {
 				double t2 = ((x3 - x1) + t1 * (x4 - x3)) / (x2 - x1);
-				if (0 < t2 && t2 < 1) {
+				if (0 <= t2 && t2 <= 1) {
 					return true;
 				} else
 					return false;
